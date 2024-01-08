4b6334cf3421aecbc4cf1868f7fd06ee777f7ffa
Cristian Garcia <crisg@sas.upenn.edu>
2024-01-08 18:13:13 -0500

    added a header and navigation bar, added functionality to filter posts according to the search text

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
+import { useState } from "react";
+import Nav from "./Nav";
+
 import postData from "./data/posts.json";
 
 function App() {
-  console.log(postData);
-  return (
-    <main>
-      <button className="btn">hi</button>
-    </main>
-  );
+    const [searchText, setSearchText] = useState("");
+
+    const handleTextChange = (e) => {
+        setSearchText(e.target.value);
+    };
+
+    return (
+        <main>
+            <Nav />
+            <h1>Travel is the only thing you buy that makes you richer</h1>
+            <label htmlFor="search" className="form-label">
+                Search posts by location...
+                <input
+                    value={searchText}
+                    onChange={handleTextChange}
+                    type="text"
+                    name="search"
+                    id="search"
+                    className="form-control"
+                />
+            </label>
+            <button>Cancel</button>
+            <div>
+                {postData.map((post) => {
+                    if (
+                        post.title
+                            .toLowerCase()
+                            .includes(searchText.toLowerCase()) ||
+                        post.location
+                            .toLowerCase()
+                            .includes(searchText.toLowerCase())
+                    ) {
+                        return (
+                            <li key={post.id}>
+                                <p>{post.title}</p>
+                                <p>{post.location}</p>
+                                <p>{post.content}</p>
+                                <img
+                                    src={`src/images/${post.location
+                                        .toLowerCase()
+                                        .replace(" ", "-")}.jpg`}
+                                    alt={`photo of ${post.location}`}
+                                />
+                                <br />
+                                <button>Go to post</button>
+                            </li>
+                        );
+                    } else return null;
+                })}
+                <p>Posts by location</p>
+            </div>
+        </main>
+    );
 }
 
 export default App;
---
+++ src/Nav.jsx
@@ -0,0 +1,13 @@
+const Nav = () => {
+    return (
+        <nav className="navbar mx-auto p-2">
+            <h1>
+                Travel <span className="script-font">Blog</span>
+            </h1>
+            <a>About</a>
+            <a>Popular posts</a>
+        </nav>
+    );
+};
+
+export default Nav;
