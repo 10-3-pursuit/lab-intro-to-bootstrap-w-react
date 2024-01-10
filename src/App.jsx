9b6c71984b5012a37c121932e87e0d4dece4bdc5
enochtagoe <enochtagoe@pursuit.org>
2024-01-09 22:43:37 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,107 @@ function App() {
   console.log(postData);
   return (
     <main>
-      <button className="btn">hi</button>
-    </main>
+   
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Travel Blog
+    <h5> </h5>
+    </a>
+    
+    
+    
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+      <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
+    </div>
+  </div>
+</nav>
+    
+    <header>
+      <h3>
+      Travel is the only thing you buy that makes you richer...
+      </h3>
+    </header>
+
+    
+
+    <br></br>
+      <button className="btn"> Posts by location</button>
+      
+      <ol class="list-group list-group">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Seattle</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Shibuya</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">2</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Chicago</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">2</span>
+  </li>
+
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">San Francisco</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">San Diego</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Sevilla</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Porto</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Burano</div>
+      
+    </div>
+    <span class="badge bg-primary rounded-pill">1</span>
+  </li>
+
+</ol>
+     
+   </main>
   );
 }
 
