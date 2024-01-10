2a9b34781a2809990ae73639429b2f0adb0ceb2e
juligarc91 <julissagarcia@pursuit.org>
2024-01-09 20:11:32 -0500

    added bootstrap CSS to Search component and Header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 const Header = () => {
   return (
-    <div>
-      <h2>
-        Travel is the only thing you buy that makes you richer...
-      </h2>
+  <div className="container-fluid bg-light">
+    <div className="row justify-content-center text-center py-5">
+      <div className="col">
+        <h2 className="script-font font-weight-bold text-dark">
+          Travel is the only thing you buy that makes you richer...
+        </h2>
+      </div>
     </div>
+  </div>
   )
 }
 
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,22 +1,26 @@
-function Search({ searchTerm, setSearchTerm, resetSearch }) {
+function Search({ searchTerm, setSearchTerm, resetSearch }) { //using these as props because other components use them as well
 
-  const handleSearchChange = (e) => {
+  const handleSearchChange = (e) => { // nothing else uses this callback fx so it's ok to move it locally inside Search component
     setSearchTerm(e.target.value);
   };
 
   return (
-    <div>
-      <label htmlFor="search">Search posts by location...</label>
-      <input
-        value={searchTerm}
-        onChange={handleSearchChange}
-        type="text"
-        name="search"
-        id="search"
-      />
-      <button onClick={resetSearch}>Cancel</button>
+    <div className="search-bar d-flex mb-3">
+        <label htmlFor="search" className="form-label">
+        Search posts by location...
+        </label>
+        <input
+            value={searchTerm}
+            onChange={handleSearchChange}
+            type="text"
+            name="search"
+            id="search"
+            className="form-control me-2"
+        />
+        <button className="btn btn-secondary" onClick={resetSearch}>Cancel</button>
     </div>
   );
 }
 
 export default Search;
+
